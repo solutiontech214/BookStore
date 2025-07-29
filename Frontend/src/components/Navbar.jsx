2cb8895b11d7f89abb030bd987889d5166d36249
solutiontech214 <139892149+solutiontech214@users.noreply.github.com>
2025-07-29 19:00:06 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -81,7 +81,7 @@ const handleScroll = () => {
               </div>
               <ul
                 tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-5 text-xl  shadow"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-5 text-xl  shadow dark:text-black"
               >
                 {navItems}
               </ul>
