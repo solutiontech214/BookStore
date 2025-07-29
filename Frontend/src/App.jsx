eceaf268cb32eab31f6efdcaed5fb5d365884fa9
solutiontech214 <139892149+solutiontech214@users.noreply.github.com>
2025-07-29 18:51:22 +0530

    Update routing logic in App and vite config
    Removed BrowserRouter from App.jsx imports and simplified route handling. Updated vite.json rewrite rule to redirect all paths to root, improving client-side routing compatibility.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './components/Home'
 import Course from './components/Store'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import StoreArea from './/components/StoreArea'
 import Signup from './components/Signup'
 export default function App() {
